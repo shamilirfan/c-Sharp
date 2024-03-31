586d185f1a5cabab832a1e64956866b185f18d43
Shamil Irfan <shamilirfan28@gmail.com>
2024-03-31 21:56:10 +0600

    Factorial

--- obj/Debug/net8.0/learn-csharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/learn-csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learn-csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb0964bd54cbad01f0f225bf34f4dc203e92c9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98e0e62b2f1640163b6a1263c86863b910a71e8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("learn-csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learn-csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
