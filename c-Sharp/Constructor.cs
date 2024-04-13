bd36f6cefefd40c2aef4f8f3cf79ca6392f6fb3b
Shamil Irfan <shamilirfan28@gmail.com>
2024-04-13 12:03:07 +0600

    Value Type vs Reference Type

---
+++ c-Sharp/Constructor.cs
@@ -0,0 +1,28 @@
+using System;
+
+class PersonClass2
+{
+    string name;
+    int age;
+    public PersonClass2(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+    public void DisplayPersonInfo()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
+class Constructor
+{
+    public static void Main(string[] args)
+    {
+        PersonClass2 person1 = new PersonClass2("Alex", 33);
+        person1.DisplayPersonInfo();
+        
+        PersonClass2 person2 = new PersonClass2("Bob", 20);
+        person2.DisplayPersonInfo();
+
+    }
+}
--- c-Sharp/obj/Debug/net8.0/c-Sharp.AssemblyInfo.cs
+++ c-Sharp/obj/Debug/net8.0/c-Sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c-Sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f75256516e037de1eb6027c36faaef1e2ac5fe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a251e4612eee6a0bc63ebeaa268a23871e18a32")]
 [assembly: System.Reflection.AssemblyProductAttribute("c-Sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c-Sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
