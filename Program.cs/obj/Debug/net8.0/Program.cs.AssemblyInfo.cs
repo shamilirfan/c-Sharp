a6928dc8f6ace5ab87d8c6152ed3227acb0a9ccd
Shamil Irfan <shamilirfan28@gmail.com>
2024-04-26 11:02:03 +0600

    Word problem solve from codeforces

--- Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
+++ Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f73596d21a3ef3045b838b3bbd41f7a6f59c9afa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6517253e179ac8f3ae735e6c93e088facc0ca97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
