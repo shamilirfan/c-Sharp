ba9f8909607143f08c39344c0a8a46492eac8d2c
Shamil Irfan <shamilirfan28@gmail.com>
2024-04-26 21:29:19 +0600

    Boy Or Girl from codeforces

--- Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
+++ Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6517253e179ac8f3ae735e6c93e088facc0ca97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6928dc8f6ace5ab87d8c6152ed3227acb0a9ccd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
