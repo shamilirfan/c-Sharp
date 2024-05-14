3840e8110bed558afc53bfab2164a372a7c9b229
Shamil Irfan <shamilirfan28@gmail.com>
2024-05-14 14:14:41 +0600

    Queue

--- Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
+++ Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d1a541b5cc0e0604418c0e051cad22b8d66d4b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69a5824a8c2a6c5f76917df66e811adf97d355e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
