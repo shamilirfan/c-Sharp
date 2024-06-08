0647304814aa17c36fc21623ecc4ff37ac5642f6
Shamil Irfan <shamilirfan28@gmail.com>
2024-06-08 20:38:50 +0600

    HQ9+

--- Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
+++ Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0707e45495a22420acf0b88c3aa4efc5bb4113ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ef491e78b8a5edbd09ca2dae7726cc2626488c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
