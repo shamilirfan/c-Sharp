2e027c56f41102f0bc4880689b49bcfa03f9c3ec
Shamil Irfan <shamilirfan28@gmail.com>
2024-06-22 16:40:12 +0600

    Wrong Subtraction from codeforces

--- Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
+++ Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4bb8ae160015bb3fe6eb39d4db2f6bc3375d1f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138ee6dc461427261c530b1d258b05f9b81e818a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
