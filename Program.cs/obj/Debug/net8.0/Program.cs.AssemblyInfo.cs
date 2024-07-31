f5d62faa15da73373a1b532d090c4560bbd0eebf
shamil Irfan <shamilirfan28@gmail.com>
2024-07-31 20:52:53 +0600

    Arrival Of The General from codeforces

--- Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
+++ Program.cs/obj/Debug/net8.0/Program.cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0730d01caf27b0cecacaf14931faced8594dbc5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1410f13af10f59937293fe4602d260bf45963760")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program.cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
