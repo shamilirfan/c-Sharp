589adab2e062328f0d04224579f8d39de9c5c40e
Shamil Irfan <shamilirfan28@gmail.com>
2024-08-29 23:43:18 -0700

    Tram from codeforces

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7a5e76bf7ac16f46a6aa3394383dca7eb8d9079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9521317580e57078a7448d0d55e595be6d76ce57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
