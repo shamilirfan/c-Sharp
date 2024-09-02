20a9f2291aa799336a9dd4e8d20beb8c2e72cf5e
Shamil Irfan <shamilirfan28@gmail.com>
2024-09-02 00:52:18 -0700

    Odd Divisor

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589adab2e062328f0d04224579f8d39de9c5c40e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+121bfb7c692fa100063ca4ea171b18a8fc0ae08e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
