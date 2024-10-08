abd251505732b711a8a5007dc3ec9a7e584bcb70
Shamil Irfan <shamilirfan28@gmail.com>
2024-10-08 08:06:15 -0700

    Minimize! from codeforces

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f0169b713d8b33b8e150fd756037d3c41d06431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98401369c2236e19f34ad59238bf4c595f84c26f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
