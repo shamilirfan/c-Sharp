78a4e17fa2c2f81ac60b0ee098f58bd185dc56bb
Shamil Irfan <shamilirfan28@gmail.com>
2024-10-13 02:26:02 -0700

    Boring Apartments from codeforces

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d3ea17d5fff17919b20f44484911c1e41e9b27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae448c4b52653223b7ebc6d90a1472341080aeb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
