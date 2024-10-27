04c971f132c96d28b4dffeeda3ad2ce5524956cd
Shamil Irfan <shamilirfan28@gmail.com>
2024-10-26 21:57:02 -0700

    Robin Helps from codeforces

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e412751cbe418056c8ca996849ea83992ab52f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2810b9bd9d33c670464b7a907f38a5dd6f20a7b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
