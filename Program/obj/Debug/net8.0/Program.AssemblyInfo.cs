29d054eaea240662ebfc33317c9da356901b9be9
Shamil Irfan <shamilirfan28@gmail.com>
2024-11-22 07:39:57 -0800

    Roman To Integer from leetCode

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125c58803b6c46b0e7b304b8751bd25640173a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0923ca93b20bc2edd2359cee7460b6b2742eb815")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
